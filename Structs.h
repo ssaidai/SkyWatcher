d7b325b4552d8d7c0d5034965e0104d28c833b64
Kevin <huang.1993737@studenti.uniroma1.it>
2024-08-09 18:54:41 +0200

    Structs update

--- Structs.h
+++ Structs.h
@@ -1,24 +1,30 @@
 #ifndef SKYWATCHER_STRUCTS_H
 #define SKYWATCHER_STRUCTS_H
 
+
 struct Position {
-    double x, y, z;
+    static const double x,
+    static const double y
 };
 
-struct Path{
-    double distance; // Distance
-    double startX, startY; // Current path's starting point
-    double destX, destY; // Current path's destination point
-    float timeNeeded; // Time needed to reach destination of the current path
-    float startedBy;  // Time passed
+struct Path {
+    Position startPoint; // Starting point
+    Position destPoint; // Destination point
+
+    static const double distance; // Distance
+    static const double criticalBatteryLevel; // When the next drone should be called
+    static const float travelTime; // Time needed to reach destination of the current path
 };
 
 struct DroneState {
     enum Enum {
-        Recharging,
-        Waiting,
-        Running,
-        Ready
+        Ready, // Drone is ready to operate
+        Charging, // Charging battery
+        Waiting, // Waiting all drone to start together
+        Arriving, // Going to the destination
+        Monitoring, // Monitoring a sector
+        Returning, // Returning to the tower
+        Offline // Drone is not operative
     };
 };
 
