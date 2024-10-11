273214b23c6c2c8703017a2f87525bfabff95053
Kevin <huang.1993737@studenti.uniroma1.it>
2024-10-11 15:48:44 +0200

    Util function

--- Utils/utils.cpp
+++ Utils/utils.cpp
@@ -1,5 +1,7 @@
 #include "utils.h"
 #include "cmath"
+#include <string>
+#include "Drone/Drone.h"
 
 // Calculate the distance between two points
 double utils::calculateDistance (Position startPoint, Position destPoint) {
@@ -7,9 +9,26 @@ double utils::calculateDistance (Position startPoint, Position destPoint) {
 }
 
 // Calculate the time needed to travel given the distance and the speed
-double utils::calculateTime (double distance, double speed) {
-    return distance * (speed / 3.6)
+float utils::calculateTime (double distance, double speed) {
+    return static_cast<float>(distance * speed);
 }
 
+// Get the percentage of battery when the next drone should be called
+double utils::getCriticalBatteryLevel(double travelTime, const double flightAutonomy) {
+    double deviation = calculateDeviation(travelTime, flightAutonomy);
+    return (2 * (travelTime / flightAutonomy * 100.0)) + deviation;
+}
 
+// Calculate the deviation from critical battery level
+double utils::calculateDeviation(double travelTime, double flightAutonomy) {
+    return std::fmod(100.0 - (2 * (travelTime / flightAutonomy * 100.0)), 14.0);
+}
 
+std::string utils::statusToJSON(Drone& drone) {
+    std::string droneState = "State: " + std::to_string(drone.getDroneState());
+    std::string position = "Position: (" + std::to_string(drone.getPosition().x) + ", "
+            + std::to_string(drone.getPosition().y) + ")";
+    std::string batteryLevel = "Battery Level: " + std::to_string(drone.getBatteryLevel()) + "%";
+    std::string critical = "Critical Point: " + std::to_string(drone.isBatteryCritical());
+    return "{" + droneState + ", " + position + ", " + batteryLevel + ", " + critical + "}";
+}
--- Utils/utils.h
+++ Utils/utils.h
@@ -1,13 +1,22 @@
 #ifndef SKYWATCHER_UTILS_H
 #define SKYWATCHER_UTILS_H
 
-#include "Drone.h"
-#include "Structs.h"
+#include <string>
+#include "Drone/Drone.h"
+#include "../Structs.h"
 
 namespace utils {
     double calculateDistance (Position startPoint, Position destPoint); // Calculate the distance between two points in metres
-    double calculateTime (double distance, double speed); // Calculate the time needed to travel in seconds
+    double getCriticalBatteryLevel(double travelTime, double flightAutonomy); // Calculate the relative battery level when the subsequent drone should be called
+    double calculateDeviation(double travelTime, double flightAutonomy); // Calculate the deviation from critical battery level
+    float calculateTime (double distance, double speed); // Calculate the time needed to travel in seconds
+    std::string statusToJSON(Drone& drone); // return a JSON containing the drone's status
 }
 
-
 #endif //SKYWATCHER_UTILS_H
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+
