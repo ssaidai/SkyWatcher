94d7f1f04652181cfefcdb965099e60c471bc636
kevin <huangkevin7986@gmail.com>
2024-10-23 21:17:26 +0200

    Drone main's threads moved to Drone.cpp

--- Drone/main.cpp
+++ Drone/main.cpp
@@ -5,58 +5,23 @@
 #include "Redis/Redis.h"
 #include "Utils/utils.h"
 
-// TODO: Implement the threads in the drone class, the main file should only generate a bunch of child processes
-
-// Drone's battery thread implementation
-void battery(Drone& drone) {
-    while (drone.getDroneState() != DroneState::Offline) {
-
-        if (drone.getDroneState() == DroneState::Charging) {
-            drone.recharge(); // It stops the thread and wait until the drone is fully charged
-        }
-        else {
-            // Battery consumption
-            drone.consumption();
-
-            // Verify if the drone should return
-            if (drone.isBatteryLow() && (drone.getPosition() == drone.getDestination())) {
-                drone.back();
-            }
-        }
-
-        // Wait a secondo before the next check
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
-}
-
-
-void status(Drone& drone) {
-    while (drone.getDroneState() != DroneState::Offline) {
-        std::string status = utils::statusToJSON(drone);
-        send_status_update(status);
-
-        std::this_thread::sleep_for(std::chrono::seconds (1));
-    }
-}
-
-void executeCMD(const std::string& command) {
-    if (command == "xxx") {
-
-    }
-}
-
 int main() {
     // Initialize a drone
     Drone drone;
 
     // Initialize redis
     RedisCommunication redisCommunication("127.0.0.1", 6379);
-    DroneClient droneClient(redisCommunication.get_redis_instance(), drone.getID());
+    DroneClient droneClient(redisCommunication.get_redis_instance());
 
     // Initialize the following threads
-    std::thread batteryThread(battery, std::ref(drone));
-    std::thread statusThread(status, std::ref(drone));
-    std::thread commandThread(&DroneClient::listen_for_commands, &droneClient, executeCMD);
+    std::thread batteryThread(&Drone::batteryThread, std::ref(drone));
+    std::thread statusThread(&Drone::status, std::ref(drone));
+    std::thread commandThread([&]() {
+        droneClient.listen_for_commands([&](const std::string& cmd) {
+            drone.executeCMD(cmd);
+        });
+    });
+
 
 
     // Wait for battery's thread to end
